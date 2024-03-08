bd9d670b7a543d54f9ef0eb002d7148ae0b8fb1a
Sunny9442 <161554542+Sunny9442@users.noreply.github.com>
2024-03-08 04:45:14 -0800

    4rth day
    4rth day of challenge

---
+++ 4rth day of challenge/app.js
@@ -0,0 +1,5 @@
+// dates : 03/08/2024
+//Hii my name is Sunny shahzad
+//how are you
+var myname = "Sunny Shahzad";
+console.log("My name is ".concat(myname, " & i am saying to everyone \"Never give up ever ever ever\""));
---
+++ 4rth day of challenge/main.ts
@@ -0,0 +1,5 @@
+let mynames: string[] =["Sunny", "Hamza", "Hammad"]
+for (let name of mynames){
+    console.log(`Hello ${mynames} would you like to some learn typescript today?`)
+    console.log("yes")
+}
