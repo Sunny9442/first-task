36346bfdd8c74d4bb8497d5d52e5ab9fdbf7416d
Sunny9442 <161554542+Sunny9442@users.noreply.github.com>
2024-03-08 10:36:05 -0800

    Delete 5th day of challenge directory

--- 5th day of challenge/app.ts
+++
@@ -1,8 +0,0 @@
-let favourites_bikes : string[]=["Yamaha", "Honda", "Kawasaki"]
-for (let i=0; i < Object.keys(favourites_bikes).length; i++){
-    console.log("This all my favourites bike " + Object.keys(favourites_bikes)[i], " is "+ favourites_bikes[Object.keys(favourites_bikes)[i]] )
-}
-let transport: string[]=["Car Lambhorgini", "Car Rolls Royce", "Car Ferrari"]
-transport.forEach(transport => {
-    console.log(`i would like to own this ${transport}`)
-});
--- 5th day of challenge/main.ts
+++
@@ -1,4 +0,0 @@
-let guests: string[]= ["Bill Gates", "Albert Einstien", "Sunny Shahzad"]
-guests.forEach(guests => {
-    console.log(`Hello Dear ${guests} Would you like to join me for Dinner`)
-});
